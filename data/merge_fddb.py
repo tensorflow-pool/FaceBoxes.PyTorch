360b523d135e1a890a56220315d374cb70d2de87
lijc08 <lijc08@ubuntu-fast.com>
2019-02-22 14:42:32 +0800

    fix:修改绘制ap

--- data/merge_fddb.py
+++ data/merge_fddb.py
@@ -3,6 +3,8 @@ import os
 
 fddbPath = os.path.expanduser("~/datasets/fddb/FDDB-folds/")
 lff = glob.glob(os.path.join(fddbPath, "*ellipseList.txt"))
+lff = sorted(lff)
+print lff
 
 with open(os.path.join("FDDB", 'img_list_ellipseList.txt'), 'w') as fileWriter:
     for f in lff:
