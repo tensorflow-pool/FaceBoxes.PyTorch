ba523d7b851fa2b80a3e5859ecbedc3241eb532b
lijc08 <lijc08@ubuntu-fast.com>
2019-02-22 15:18:47 +0800

    fix

--- face-eval/fddb_evaluate.sh
+++ face-eval/fddb_evaluate.sh
@@ -1,1 +1,1 @@
-./fddb_evaluate -a ../data/FDDB/img_list_ellipseList.txt -d ../eval/FDDB_dets.txt -f 0 -l ../data/FDDB/img_list.txt -i ../data/FDDB/images/ -r ./detections/fddb/MaysaFaceboxes
+./fddb_evaluate -a ../data/FDDB/img_list_ellipseList.txt -d ../eval/maysa_FDDB_dets.txt -f 0 -l ../data/FDDB/img_list.txt -i ../data/FDDB/images/ -r ./detections/fddb/MaysaFaceboxes
